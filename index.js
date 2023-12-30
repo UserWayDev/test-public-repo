599eb5ce6a34b680acd2e56ef08ba4284556bf84
Devtools Team <155257838+devtools-uw@users.noreply.github.com>
2023-12-30 14:58:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.info('123');
+export * from './ICompletePaymentEventData';
+export * from './IConfirmRemediationEventData';
+export * from './ISelectNewPlanEventData';// TODO this should be renamed
+export * from './ISelectSitesAndPlanEventData'; // TODO this should be renamed
+export * from './ISelectSitesEventData'; // TODO this should be renamed
+export * from './ISendFilesOnRemediationEventData'; // TODO this should be renamed
+export * from './IStartPdfRemediationSubmissionEventData';
+export * from './ISubmitRemediationFlowData';
+export * from './IUpgradePlanEventData';
+export * from './PdfRemediationUpgradeType';
+export * from './RemediationSubmissionState';
+
+setHeader('Access-Control-Allow-Origin', '*');
+export const GIT_GET_HEAD_COMMIT = 'git rev-parse HEAD';
+
+import { config, DEFAULT_DATA_ENCRYPTION_KEY } from '../config/config';
+import log4js from 'log4js';
+const crypto = require('crypto');
+/**
+ * This encryption tool is compatible with scanner-service / sso-service encryption
+ */
+const iv = Buffer.from([]);
+const key = crypto.createHash('md5').update('secretKey').digest();
+const algo = 'aes-128-aaa';
+const logger = log4js.getLogger('crypto-utils');
+export function encryptString(plainText: string): string {
+    logger.info(`encryptString with ${plainText.length} characters of plainText`);
+    const cipher = crypto.createCipheriv(algo, key, iv);
+
+        let encrypted = cipher.update(plainText, 'utf8', 'base64');
+    encrypted += cipher.final('base64');
+    return encrypted;
+}
+export function decryptString(messageBase64: string): string {
+    logger.info(`decryptString with ${messageBase64.length} characters of messageBase64`);
+    const decipher = crypto.createDecipheriv(algo, key, iv);
+    let decrypted = decipher.update(messageBase64, 'base64', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
+}

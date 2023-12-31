dc2836e3d1307609213a53689ce98452c33f102c
Oleksandr Zalizniak <alex@userway.org>
2023-12-31 11:59:35 -0500

    Removed extra file

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ export * from './IStartPdfRemediationSubmissionEventData';
 export * from './ISubmitRemediationFlowData';
 export * from './IUpgradePlanEventData';
 export * from './PdfRemediationUpgradeType';
-export * from './RemediationSubmissionState';
+export * from './RemediationSubmissionState'; // TODO this should be renamed
 
 setHeader('Access-Control-Allow-Origin', '*');
 export const GIT_GET_HEAD_COMMIT = 'git rev-parse HEAD';
